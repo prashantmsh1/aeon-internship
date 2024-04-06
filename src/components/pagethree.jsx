ec54e2fbf031dd5b8e150359a341de3ed89c26ce
prashant <prashantkr.msh@gmail.com>
2024-04-06 23:17:10 +0530

    almost ready

--- src/components/pagethree.jsx
+++ src/components/pagethree.jsx
@@ -5,13 +5,13 @@ export default function PageThree({ page, setpage }) {
     return (
         <div>
 
-            <div className="grid grid-cols-2 mx-auto w-9/10">
+            <div className="grid grid-rows-2 mx-auto md:grid-cols-2 w-9/10">
                 <div className="">
-                    <img src={pully} alt="" />
+                    <img src={pully} alt="" className="mx-auto " />
                 </div>
                 <div className="grid grid-flow-row ">
                     <h1 className="my-auto text-4xl font-bold text-left ">You're in the right place</h1>
-                    <p className="-mt-24 text-lg font-semibold text-left ">
+                    <p className="text-lg font-semibold text-left md:-mt-24 ">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nobis maiores perspiciatis illum laudantium at placeat. Ipsa nostrum voluptate cum laudantium totam cupiditate ab! Facere distinctio, omnis ullam adipisci sit veniam hic. A iste molestias quae voluptatem omnis labore possimus!
                     </p>
                 </div>
